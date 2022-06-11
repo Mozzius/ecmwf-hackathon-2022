63a336c782a7d2813a2dab6e3ca762ce0b7565d4
Stuart Evans <th3.black.st1g@gmail.com>
2022-06-11 20:58:23 +0100

    added wind speed and direction to csv

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import netCDF4
 import pandas as pd
 import numpy as np
+import math
 
 
 def find_closest_lat_lon_index(lat,lon,lat_arr,lon_arr):
@@ -38,12 +39,30 @@ def extract_data(file_path,lat,lon):
     f.close()
     
     return return_df
+
+def convertU10V10toSpdDir(pandas_df):
+    # inplace convert u10 & v10 to wind speed and direction
+    #pandas_df["wind_speed"] = math.sqrt(math.pow(pandas_df["u10"],2) * math.pow(pandas_df["v10"],2))
+    #pandas_df["wind_dir_rad"] = math.fmod(360.0+math.atan2(pandas_df["u10"],pandas_df["v10"]),360)
+    print(pandas_df.shape)
     
+    tmp_wind_speed = np.zeros(shape=(pandas_df.shape[0]))
+    tmp_wind_dir = np.zeros(shape=(pandas_df.shape[0]))
+    
+    for j in range(pandas_df.shape[0]):
+        tmp_wind_speed[j] = math.sqrt(math.pow(pandas_df.at[j,"u10"],2) * math.pow(pandas_df.at[j,"v10"],2))
+        tmp_wind_dir[j] = math.fmod(360.0+math.atan2(pandas_df.at[j,"u10"],pandas_df.at[j,"v10"]),360)
+    pandas_df["wind_speed"] = tmp_wind_speed
+    pandas_df["wind_dir_rad"] = tmp_wind_dir
+    
+    return pandas_df   
 if __name__ == "__main__":
     LAT = 40.0
     LON = 0.0
     data_frame = extract_data("wind-data.nc",LAT,LON)
-    data_frame.to_csv("extracted_wind_{0}N{0}E.csv".format(LAT,LON))
+    #data_frame.to_csv("extracted_wind_{0}N{0}E.csv".format(LAT,LON))
+    data_frame = convertU10V10toSpdDir(data_frame)
+    data_frame.to_csv("extracted_wind_with_spd_dir_{0}N{0}E.csv".format(LAT,LON))
     print(data_frame.head())
 
 
